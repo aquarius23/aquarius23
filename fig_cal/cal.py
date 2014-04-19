6651f3753ea56853e78658df3ec6cdae5248b9aa
haibo.hu <haibo.hu@archermind.com>
2014-04-19 18:18:54 +0800

    add pinan cal

--- fig_cal/cal.py
+++ fig_cal/cal.py
@@ -17,5 +17,29 @@ def cal_month_money(money, month, ratio):
 	month_money = (money * ratio * factor) / (factor - 1)
 	return month_money
 
-money = cal_month_money(500000, 12*30, 6.55)
-print money
+def cal_money_fund_oneday(money, ratio):
+	ratio = float(ratio) / 36500
+	return money * ratio
+
+def cal_money_fund_day(money, ratio):
+	return money + cal_money_fund_oneday(money, ratio)
+
+def cal_money_fund_month(money, ratio):
+	for i in range(0, 30):
+		money = cal_money_fund_day(money, ratio)
+	return money
+
+def cal_pinan_money(money, month, ratio, fund_ratio):
+	receive = cal_month_money(money, month, ratio)
+	total = receive
+	for i in range(1, month):
+		fund = cal_money_fund_month(total, fund_ratio)
+		total = fund + receive
+	return total
+
+def cal_pinan_ratio(money, month, ratio, fund_ratio):
+	money = cal_pinan_money(money, month, ratio, fund_ratio) - money
+	return (money * 12) / (month * 100)
+
+ratio = cal_pinan_ratio(10000, 36, 8.61, 5)
+print ratio

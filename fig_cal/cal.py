d4924e20380fdadec1d7335858cd96903201785a
haibo.hu <haibo.hu@archermind.com>
2014-04-20 13:43:24 +0800

    add diff_cmp

--- fig_cal/cal.py
+++ fig_cal/cal.py
@@ -32,5 +32,19 @@ def cal_pinan_ratio(money, month, ratio, fund_ratio):
 	money = cal_pinan_money(money, month, ratio, fund_ratio) - money
 	return (money * 12) / (month * 100)
 
-ratio = cal_pinan_ratio(10000, 36, 8.61, 5)
-print ratio
+def cal_fix_month_invest(money, month, ratio):
+	sum = money
+	for i in range(1, month):
+		sum = money + cal_money_fund_month(sum, ratio)
+	return sum
+
+def diff_cmp(total_money, reserve_money, bank_ratio, invest_ratio, year):
+	bank_orig_month = cal_month_money(total_money, year * 12, bank_ratio)
+	bank_before_month = cal_month_money(total_money - reserve_money, year * 12, bank_ratio)
+	bank_orig = bank_orig_month * year * 12
+	bank_before = bank_before_month * year * 12
+	invest_orig = reserve_money * cal_ratio(year, float(invest_ratio) / 100)
+	invest_before = cal_fix_month_invest(bank_orig_month - bank_before_month, year * 12, invest_ratio)
+	return (invest_orig - bank_orig) - (invest_before - bank_before - reserve_money)
+diff = diff_cmp(500000, 200000, 6.55,  8.61, 30)
+print diff

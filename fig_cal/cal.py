ea68a351c65a963aa3e64336835c0167b4a9caf2
haibo.hu <haibo.hu@visionure.com>
2014-05-14 12:05:11 +0800

    fix fig_cal buf

--- fig_cal/cal.py
+++ fig_cal/cal.py
@@ -34,17 +34,18 @@ def cal_pinan_ratio(money, month, ratio, fund_ratio):
 
 def cal_fix_month_invest(money, month, ratio):
 	sum = money
-	for i in range(1, month):
-		sum = money + cal_money_fund_month(sum, ratio)
+	for i in range(0, month):
+		sum = cal_money_fund_month(sum, ratio) + money
 	return sum
 
-def diff_cmp(total_money, reserve_money, bank_ratio, invest_ratio, year):
-	bank_orig_month = cal_month_money(total_money, year * 12, bank_ratio)
-	bank_before_month = cal_month_money(total_money - reserve_money, year * 12, bank_ratio)
-	bank_orig = bank_orig_month * year * 12
-	bank_before = bank_before_month * year * 12
-	invest_orig = reserve_money * cal_ratio(year, float(invest_ratio) / 100)
-	invest_before = cal_fix_month_invest(bank_orig_month - bank_before_month, year * 12, invest_ratio)
-	return (invest_orig - bank_orig) - (invest_before - bank_before - reserve_money)
-diff = diff_cmp(500000, 200000, 6.55,  8.61, 30)
+def cal_fix_reply_invest(total_money, money, month, ratio):
+	sum = total_money
+	for i in range(0, month):
+		sum = cal_money_fund_month(sum, ratio) - money
+	return sum
+
+def diff_cmp(money, bank_ratio, invest_ratio, year):
+	month_money = cal_month_money(money, year * 12, bank_ratio)
+	return cal_fix_reply_invest(money, month_money, year * 12, invest_ratio)
+diff = diff_cmp(100000, 6.55, 8.61, 1)
 print diff

5c60f6964dd5e81249a172c4dbdb322b67607a8e
haibo.hu <haibo.hu@visionure.com>
2014-09-17 16:47:03 +0800

    add jidu loop

--- fig_quants/stockmanager.py
+++ fig_quants/stockmanager.py
@@ -24,13 +24,17 @@ class stockmanager():
 		return self.parser.get_stock_list()
 
 	def get_stock_index(self, name):
-		today = get_date()
-		start = stockconfig.FIG_START_DAY
-		print today
-		print start
+		today = get_date().split('-')
+		start = stockconfig.FIG_START_DAY.split('-')
+		start_year = string.atoi(start[0])
+		start_jidu = (string.atoi(start[1]) + 2) / 3
+		end_year = string.atoi(today[0])
+		end_jidu = (string.atoi(today[1]) + 2) / 3
+		while True:
+			#
+			if(start_year == end_year) and (start_jidu == end_jidu):
+				break;
+			start_year, start_jidu = next_jidu(start_year, start_jidu)
 
 x = stockmanager()
 x.get_stock_index('600015')
-for i in range(1, 12 + 1):
-	jidu = (i + 2) / 3
-

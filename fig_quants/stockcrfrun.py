567e6692687247063915016febe054bf2f1cda05
haibo.hu <haibo.hu@visionure.com>
2014-11-28 14:43:12 +0800

    add stockcrfrun.py

---
+++ fig_quants/stockcrfrun.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#!coding=utf-8
+import stockrun
+
+class stockcrfrun(stockrun.stockrun):
+	chain = 5
+	crftag = []
+	crffeature = []
+
+	def reset(self):
+		self.crftag = []
+		self.crffeature = []
+
+	def set_chain_size(self, size):
+		self.chain = size
+
+	def set_tag_feature(self, tag, feature):
+		self.crftag.append(tag[:])
+		self.crffeature.append(feature[:])
+		if len(self.crftag) > self.chain:
+			self.crftag.remove(self.crftag[0])
+			self.crffeature.remove(self.crffeature[0])
+		if len(self.crftag) == self.chain:
+			print '------------------'
+			print self.crftag
+			print self.crffeature

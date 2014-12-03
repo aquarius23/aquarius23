67835b11058e2afb63fea51d849d87510f58b9aa
haibo.hu <haibo.hu@visionure.com>
2014-12-03 10:38:55 +0800

    cleanup source code

--- fig_quants/test_rule.py
+++ fig_quants/test_rule.py
@@ -9,50 +9,6 @@ class myemu(stockcrfrun.stockcrfrun):
 	mycrftag = stockcrf.stockcrftagger()
 	mycrftag.open_model('crf.bin')
 
-	def get_state(self, index, exchange, kline):
-		next = self.fix_index(1)
-		today = kline[index][0]
-		if today >= 1:
-			today = 1
-		elif today <= -1:
-			today = -1
-		else:
-			today = 0
-
-		tomorrow = kline[next][0]
-		if tomorrow >= 1:
-			tomorrow = 1
-		elif tomorrow <= -1:
-			tomorrow = -1
-		else:
-			tomorrow = 0
-
-		return str(tomorrow)
-
-	def feature_kdj(self, exchange, index, kdj):
-		adj1 = self.fix_index(-1)
-		ret = []
-		j =  kdj[index][2]
-		if j < 20:
-			ret.append('j=-1')
-		elif j >80:
-			ret.append('j=1')
-		else:
-			ret.append('j=0')
-		return ret
-
-	def feature_macd(self, exchange, index, macd):
-		ret = []
-		macd = macd[index][2]
-		if macd >= 0:
-			ret.append('macd=1')
-		else:
-			ret.append('macd=-1')
-		return ret
-
-	def feature_boll(self, exchange, index, macd):
-		return []
-
 	def filter_kdj(self, exchange, index, kdj):
 		if kdj[index][2] > 0:
 			return 0

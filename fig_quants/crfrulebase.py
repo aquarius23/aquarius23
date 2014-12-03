9f048836e36ed40a13f43f7e6cbda230faff3624
haibo.hu <haibo.hu@visionure.com>
2014-12-03 10:00:23 +0800

    add crfrulebase.py

---
+++ fig_quants/crfrulebase.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#!coding=utf-8
+
+class crfrulebase():
+	def get_state(self, index, exchange, kline):
+		return []
+
+	def feature_macd(self, exchange, index, macd):
+		return []
+
+	def feature_kdj(self, exchange, index, kdj):
+		return []
+
+	def feature_boll(self, exchange, index, boll):
+		return []
+
+	def feature_ma(self, exchange, index, ma):
+		return []
+
+	def feature_volume_ma(self, exchange, index, volume_ma):
+		return []
+
+	def feature_price_range(self, exchange, index, low, high):
+		return []
+
+	def feature_volume_range(self, exchange, index, low, high):
+		return []
+
+	def feature_closeprice_range(self, exchange, index, low, high):
+		return []
+
+	def feature_kline(self, exchange, index, kline):
+		return []
+
+	def feature_exchange(self, index, exchange):
+		return []

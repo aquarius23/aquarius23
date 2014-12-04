476632ccec3007b663e34ff14b2e9551244db8cc
haibo.hu <haibo.hu@visionure.com>
2014-12-04 09:03:52 +0800

    test_crf.py

--- fig_quants/test_crf.py
+++ fig_quants/test_crf.py
@@ -7,15 +7,15 @@ import stockcrf
 manager = stockmanager.stockmanager()
 trainer = stockcrf.stockcrftrainer()
 list = manager.get_stock_list()
-list = ['600015','600030','600036','600050','600029']
-list = ['sh000001']
+run = stockcrfrun.stockcrfrun()
+#list = ['600015','600030','600036','600050','600029']
+#list = ['002204']
 for index in list:
 	print index
 	e = manager.get_stock_index(index)
 	if e == []:
 		continue
-	run = stockcrfrun.stockcrfrun()
 	run.feed(e)
 	for tag, feature in run.tag_feature():
 		trainer.set_tag_feature(tag, feature)
-	trainer.get_model('crf.bin')
+trainer.get_model('crf.bin')

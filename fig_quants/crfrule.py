bfbf82e32eea76bd70d859f62cf273a2714066f7
haibo.hu <haibo.hu@visionure.com>
2014-12-09 14:46:18 +0800

    remove kdj1_trend

--- fig_quants/crfrule.py
+++ fig_quants/crfrule.py
@@ -66,21 +66,12 @@ class crfrule(crfrulebase.crfrulebase):
 		kdj1_sort = []
 		kdj2_sort = []
 		kdj3_sort = []
-		kdj1_trend = []
 		kd_trend = []
 		for i in range(-5,1):
 			adj = self.fix_index(i)
 			adj_1 = self.fix_index(i-1)
-			y = kdj[adj_1][2]
-			t = kdj[adj][2]
 			k = kdj[adj][0]
 			d = kdj[adj][1]
-			if t > y:
-				kdj1_trend.append('+')
-			elif t < y:
-				kdj1_trend.append('-')
-			else:
-				kdj1_trend.append('=')
 
 			if k > d:
 				kd_trend.append('+')
@@ -94,7 +85,6 @@ class crfrule(crfrulebase.crfrulebase):
 		ret.append(self.build_sort_feature('kdj1_s', kdj1_sort))
 		ret.append(self.build_sort_feature('kdj2_s', kdj2_sort))
 		ret.append(self.build_sort_feature('kdj3_s', kdj3_sort))
-		ret.extend(self.build_feature('kdj1_t', kdj1_trend))
 		ret.extend(self.build_feature('kd_t', kd_trend))
 		return ret
 

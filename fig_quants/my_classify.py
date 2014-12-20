f2ce5a3de016314df3b90483d54e24c6ead384c5
haibo.hu <haibo.hu@acumulus9.com>
2014-12-20 16:07:40 +0800

    rebuild my_classify.py

--- fig_quants/my_classify.py
+++ fig_quants/my_classify.py
@@ -20,36 +20,39 @@ def c_continue(index, size):
 
 def c_break(index, size):
 	return 0
-manager = stockmanager.stockmanager()
-sort = stocksort.get_sort(0)
-list = get_max_filter(1, sort)
-#stockmodel.get_stock_modle(list, 'crftemp.bin', c_continue, c_break)
 
 def tag_filter(index, size, tag, p, m):
 	ret = string.atoi(tag[-1])
-	if ret >= 3 and p > 0.3 and m > 0.8:
-		print str(p) + '  ' + str(m)
+	if ret >= 3 and p > 0.2 and m > 0.7:
+		#print str(p) + '  ' + str(m)
 		return 1
 	return 0
+
+def cal_cos(model, ref, list):
+	output = []
+	for index in list:
+		index = index[0]
+		arg = []
+		arg.append(index)
+		ret = model.get_result(arg)
+		print ret
+		cos = arccos.vec_acos(ref, ret)
+		x = []
+		x.append(index)
+		x.append(cos)
+		output.append(x)
+		print 'cos = ' + str(cos)
+	output.sort(cmp = lambda x,y: cmp(x[1],y[1]))
+	return output
+
+manager = stockmanager.stockmanager()
+sort = stocksort.get_sort(0)
+list = get_max_filter(20, sort)
+#stockmodel.get_stock_modle(list, 'crftemp.bin', c_continue, c_break)
 
 model = stockmodel.stockmodeltag()
 model.open_model('crftemp.bin', tag_filter)
 zxzj = model.get_result(['600030'])
-output = []
-for index in sort:
-	index = index[0]
-	print '------------'
-	arg = []
-	arg.append(index)
-	ret = model.get_result(arg)
-	print ret
-	cos = arccos.vec_acos(zxzj, ret)
-	x = []
-	x.append(index)
-	x.append(cos)
-	output.append(x)
-	print 'cos='+str(cos)
+
+print cal_cos(model, zxzj, list)
 model.close_model()
-output.sort(cmp = lambda x,y: cmp(x[1],y[1]))
-for x in output:
-	print x[0]+':'+str(x[1])

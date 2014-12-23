363765d8a4ecec1e54ebc6a49bee69b0b8190d67
haibo.hu <haibo.hu@visionure.com>
2014-12-23 10:16:19 +0800

    restruct test_crf.py

--- fig_quants/test_crf.py
+++ fig_quants/test_crf.py
@@ -7,9 +7,10 @@ def c_continue(index, size):
 	return 0
 
 def c_break(index, size):
+	if index > (size * 2 / 3):
+		return 1
 	return 0
 
-sort = stocksort.get_sort_cl(0)
-print sort
-#stockmodel.get_stock_modle(list, 'crftemp.bin', c_continue, c_break)
+list = stocksort.get_sort_cl(0)
+stockmodel.get_stock_modle(list, 'crftest.bin', c_continue, c_break)
 
